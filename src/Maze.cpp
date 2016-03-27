8ef7f21fa7c61f7855719c861c6e609a328dae7a
PhanindraKothoori <PhanindraKothoori@missionrnd.com>
2016-03-27 14:39:06 +0530

    Submit Commit

--- src/Maze.cpp
+++ src/Maze.cpp
@@ -34,9 +34,43 @@ more parameters .
 */
 
 #include<stdlib.h>
-
+int checkpath(int *maze, int rows, int columns, int x, int y, int x2, int y2, int prev){
+	if (x == x2 && y == y2){
+		return 1;
+	}
+	if (x > rows || y > columns){
+		return 0;
+	}
+	int right = x*columns + y + 1, down = (x + 1)*columns + y, left = (x)*columns + y - 1, up = (x - 1)*columns + y, current = x*columns + y;
+	int arr[] = { right, down, left, up };
+	if (maze[right] == 1 && y + 1 < columns){
+		return checkpath(maze, rows, columns, x, y + 1, x2, y2, current);
+	}
+	else if (maze[down] == 1 && x + 1 < rows){
+		return checkpath(maze, rows, columns, x + 1, y, x2, y2, current);
+	}
+	else if (maze[left] == 1 && prev != left){
+		return checkpath(maze, rows, columns, x, y - 1, x2, y2, current);
+	}
+	else  if (maze[up] == 1 && prev != up){
+		return checkpath(maze, rows, columns, x - 1, y, x2, y2, current);
+	}
+	else{
+		return 0;
+	}
+}
 
 int path_exists(int *maze, int rows, int columns, int x1, int y1, int x2, int y2)
 {
-	return 1;
+	if (maze[x2*columns + y2]){
+		if (x1 >= 0 && x2 >= 0 && y1 >= 0 && y2 >= 0 && x1 < rows && x2 < rows && y1 < columns && y2 < columns){
+			return checkpath(maze, rows, columns, x1, y1, x2, y2, 0);
+		}
+		else{
+			return 0;
+		}
+	}
+	else{
+		return 0;
+	}
 }
